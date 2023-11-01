a247cc47230cf23661fa84997fa72701723e7115
ss_shankar <ssshankar1911@gmail.com>
2023-11-01 22:18:04 +0530

    added bootstrap and nav bar

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
@@ -9,3 +9,7 @@ def login():
 @auth.route("/signup")
 def signup():
 	return render_template("signup.html")
+
+@auth.route("/logout")
+def logout():
+	return redirect(url_for("views.home"))
