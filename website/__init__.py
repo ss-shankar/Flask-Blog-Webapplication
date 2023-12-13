673f8d509715b31ff1ef43376f74a3439a43baa2
ss_shankar <ssshankar1911@gmail.com>
2023-12-13 16:14:11 +0530

    demo

--- website/__init__.py
+++ website/__init__.py
@@ -3,10 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = "MYKEY"
+	app.config['SQL_ALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+	db.init_app(app)
+
 
 
 	from .views import views
@@ -15,4 +21,11 @@ def create_app():
 	app.register_blueprint(views, url_prefix="/")
 	app.register_blueprint(auth, url_prefix="/")
 
+	create_database(app)
+
 	return app
+
+def create_database(app):
+	if not path.exists("website/" + DB_NAME):
+		db.create_all(app=app)
+		print("CReated Database!")
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+	eamil = request.form.get("email")
+	password = request.form.get("password")
 	return render_template("login.html")
 
-@auth.route("/signup")
+@auth.route("/signup",  methods=['GET', 'POST'])
 def signup():
+	eamil = request.form.get("email")
+	username = request.form.get("username")
+	password_1 = request.form.get("password_1")
+	password_2 = request.form.get("password_2")
 	return render_template("signup.html")
 
-@auth.route("/logout")
+@auth.route("/logout", methods=['GET', 'POST'])
 def logout():
 	return redirect(url_for("views.home"))
